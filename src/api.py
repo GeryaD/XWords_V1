7b2bc1cfd931e1f08620a735c8c1c0c8a76d75e3
GeryaD <46264191+GeryaD@users.noreply.github.com>
2023-12-15 21:33:37 +0300

    ;

--- src/api.py
+++ src/api.py
@@ -67,4 +67,4 @@ async def check(request: Request, name:str, number:str):
 @app.websocket("/ws/v1/{name}/{number}")
 async def websocket_endpoint(websocket: WebSocket, name:str, number:str):
     websocket.accept()
-    await games[number].add_Player(name=name, connection=websocket)
+    await games[number].add_Player(name=name.upper, connection=websocket)
